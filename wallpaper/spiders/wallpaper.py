5de262efb5471227f79865874d62f6b6643a897e
DingDing <1010570970@qq.com>
2019-03-30 23:17:42 +0800

    updata

--- wallpaper/spiders/wallpaper.py
+++ wallpaper/spiders/wallpaper.py
@@ -1,11 +1,43 @@
 # -*- coding: utf-8 -*-
+import re
+from contextlib import closing
+
 import scrapy
+from scrapy import Spider, FormRequest, Request
+import requests
 
 
-class WallpaperSpider(scrapy.Spider):
+class WallpaperSpider(Spider):
     name = 'wallpaper'
-    allowed_domains = ['wallpaper.com']
-    start_urls = ['http://wallpaper.com/']
+    start_urls = 'https://alpha.wallhaven.cc/search?q=4k&categories=000&purity=100&atleast=3840x2160&sorting=relevance&order=desc&page=2'
+    def start_requests(self):
+        for i in range(3):
+            start_urls = 'https://alpha.wallhaven.cc/search?q=4k&categories=000&purity=100&atleast=3840x2160&sorting=relevance&order=desc&page=%d'% (i+1)
 
-    def parse(self, response):
-        pass
+            yield FormRequest(start_urls,callback=self.get_image)
+
+    def get_image(self,response):
+        urls = response.xpath('//li/figure/a/@href').extract()
+        print(len(urls))
+        for url in urls:
+            print(url)
+        # url = 'https://alpha.wallhaven.cc/wallpaper/593940'
+            yield Request(url,callback=self.download_url)
+    def download_url(self,response):
+        url = response.xpath('//img[@id="wallpaper"]/@src').extract()
+        print(len(url))
+
+        # yield FormRequest("https:"+url,callback=self.download)
+        print(url[0])
+        titlr = re.findall("-(\d+).",url[0])
+        print(titlr[0])
+
+        with closing(requests.get(url='https:'+url[0], stream=True, verify=False)) as r:
+            with open(str('download/'+titlr[0])+'.jpg', 'ab+') as f:
+                f.write(r.content)
+                # for chunk in r.iter_content(chunk_size=2048):
+                #     if chunk:
+                #         f.write(chunk)
+                #         f.flush()
+
+
